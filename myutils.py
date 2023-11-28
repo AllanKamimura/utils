2d2535641b979bb7753a0b9f576b437b2666ab3a
Allan Kamimura <allan_kamimura@hotmail.com>
2023-11-28 17:12:31 -0300

    read_sheets add kwargs option

--- myutils.py
+++ myutils.py
@@ -1,24 +1,36 @@
 import numpy as np
-np.set_printoptions(precision = 4, threshold = 1000, linewidth = np.inf)
+np.set_printoptions(precision = 4, 
+                    threshold = 1000, 
+                    linewidth = np.inf, 
+                    formatter = {"float_kind":"{:7.4f}".format})
+
 
 import matplotlib.pyplot as plt
 plt.rcParams['lines.linewidth'] = 3
 plt.rcParams['figure.figsize']  = (10, 8)
 
-import seaborn as sns
-sns.set_theme(
-    palette = "bright", 
-    font_scale = 1.1,
-    )
+try:
+    import seaborn as sns
+   
+    sns.set_theme(
+        palette = "bright", 
+        font_scale = 1.1,
+        )
+except:
+    pass
 
 import plotly.express as px
 
 import pandas as pd
-def read_sheets(url):
+def read_sheets(url, decimal = ",", **kwargs):
     url = url.replace('/edit#gid=', '/export?format=tsv&gid=')
     df  = pd.read_csv(url,
-                      sep = "\t")
+                      sep = "\t",
+                      decimal = decimal,
+                      **kwargs)
     return df
+pd.set_option('display.max_columns', 50)
+
 
 from datetime import datetime
 from glob     import glob
@@ -27,6 +39,7 @@ from PIL      import Image
 
 import os
 import re
+
 def download_drive(url, unzip = True):
     id = re.findall("d/([\S]*)/", url)[0]
 
